886bb2f826e155d0e893ff62062a14c21a4b023a
zizwiz <zizwiz@engineer.com>
2021-05-03 18:04:44 +0100

    Initial release to public

---
+++ scrollbar_graph/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace scrollbar_graph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_fill = new System.Windows.Forms.Button();
+            this.btn_zoom = new System.Windows.Forms.Button();
+            this.txtbx_min = new System.Windows.Forms.TextBox();
+            this.txtbx_max = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_min_value = new System.Windows.Forms.Label();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.lbl_max_value = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(776, 365);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseDown);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(679, 398);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(108, 38);
+            this.btn_exit.TabIndex = 1;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // btn_fill
+            // 
+            this.btn_fill.Location = new System.Drawing.Point(12, 388);
+            this.btn_fill.Name = "btn_fill";
+            this.btn_fill.Size = new System.Drawing.Size(108, 38);
+            this.btn_fill.TabIndex = 2;
+            this.btn_fill.Text = "Fill";
+            this.btn_fill.UseVisualStyleBackColor = true;
+            this.btn_fill.Click += new System.EventHandler(this.btn_fill_Click);
+            // 
+            // btn_zoom
+            // 
+            this.btn_zoom.Location = new System.Drawing.Point(246, 398);
+            this.btn_zoom.Name = "btn_zoom";
+            this.btn_zoom.Size = new System.Drawing.Size(108, 38);
+            this.btn_zoom.TabIndex = 3;
+            this.btn_zoom.Text = "Show";
+            this.btn_zoom.UseVisualStyleBackColor = true;
+            this.btn_zoom.Click += new System.EventHandler(this.btn_zoom_Click);
+            // 
+            // txtbx_min
+            // 
+            this.txtbx_min.Location = new System.Drawing.Point(360, 406);
+            this.txtbx_min.Name = "txtbx_min";
+            this.txtbx_min.Size = new System.Drawing.Size(67, 22);
+            this.txtbx_min.TabIndex = 4;
+            this.txtbx_min.Text = "0";
+            this.txtbx_min.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtbx_max
+            // 
+            this.txtbx_max.Location = new System.Drawing.Point(459, 406);
+            this.txtbx_max.Name = "txtbx_max";
+            this.txtbx_max.Size = new System.Drawing.Size(67, 22);
+            this.txtbx_max.TabIndex = 5;
+            this.txtbx_max.Text = "100";
+            this.txtbx_max.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(433, 409);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "to";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(314, 444);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Draw with mouse to ZOOM";
+            // 
+            // lbl_min_value
+            // 
+            this.lbl_min_value.AutoSize = true;
+            this.lbl_min_value.Location = new System.Drawing.Point(676, 453);
+            this.lbl_min_value.Name = "lbl_min_value";
+            this.lbl_min_value.Size = new System.Drawing.Size(42, 17);
+            this.lbl_min_value.TabIndex = 8;
+            this.lbl_min_value.Text = "value";
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(12, 432);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(108, 38);
+            this.btn_reset.TabIndex = 9;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_fill_Click);
+            // 
+            // lbl_max_value
+            // 
+            this.lbl_max_value.AutoSize = true;
+            this.lbl_max_value.Location = new System.Drawing.Point(746, 453);
+            this.lbl_max_value.Name = "lbl_max_value";
+            this.lbl_max_value.Size = new System.Drawing.Size(42, 17);
+            this.lbl_max_value.TabIndex = 10;
+            this.lbl_max_value.Text = "value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 479);
+            this.Controls.Add(this.lbl_max_value);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.lbl_min_value);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbx_max);
+            this.Controls.Add(this.txtbx_min);
+            this.Controls.Add(this.btn_zoom);
+            this.Controls.Add(this.btn_fill);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Chart Test";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button btn_fill;
+        private System.Windows.Forms.Button btn_zoom;
+        private System.Windows.Forms.TextBox txtbx_min;
+        private System.Windows.Forms.TextBox txtbx_max;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_min_value;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Label lbl_max_value;
+    }
+}
+
---
+++ scrollbar_graph/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace scrollbar_graph
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_fill_Click(object sender, EventArgs e)
+        {
+            int blockSize = 100; //number of points in x-axis
+
+            // generates random data (i.e. 30 * blockSize random numbers)
+            Random rand = new Random();
+            var valuesArray = Enumerable.Range(0, blockSize * 30).Select(x => rand.Next(1, 10)).ToArray();
+
+            // clear the chart
+            chart1.Series.Clear();
+
+            Series series = chart1.Series.Add("");
+            ChartArea ca = chart1.ChartAreas[series.ChartArea];  // get hold of chart
+
+            //type of chart
+            series.ChartType = SeriesChartType.FastLine;  //not sure why I need this but if not I get bar chart
+
+
+            //Populate with the random data we made
+            for (int i = 0; i < valuesArray.Length; i++)
+                series.Points.AddXY(i, valuesArray[i]);
+            
+
+            // set view range to [0,max]
+            ca.AxisX.Minimum = 0;
+            ca.AxisX.Maximum = valuesArray.Length;
+
+            // enable autoscroll
+            ca.AxisX.ScaleView.Zoomable = true;
+            ca.CursorX.AutoScroll = true;
+            ca.CursorX.IsUserSelectionEnabled = true; //adds reset button on left
+
+            // Zoom to first 100 x-axis points
+            ca.AxisX.ScaleView.SizeType = DateTimeIntervalType.Number;
+            int position = 0;
+            int size = blockSize; //adjust this to show how many are first shown
+            ca.AxisX.ScaleView.Zoom(position, size);
+
+            // We show the reset button on left of the scroll bars but if you want to not show it then you can
+            // disable it as shown commented out below 
+            //chartArea.AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.SmallScroll; //remove reset button
+
+            // set scrollbar small change to blockSize (e.g. 100)
+            ca.AxisX.ScaleView.SmallScrollSize = blockSize;
+
+            chart1.Legends.Clear(); //remove all legends
+
+            GC.Collect(); //Cleanup in case of memory leak.
+        }
+
+        private void btn_zoom_Click(object sender, EventArgs e)
+        {
+            double min_val = double.Parse(txtbx_min.Text) + 1; //Zero based
+            double max_val = double.Parse(txtbx_max.Text) + 1; //Zero based
+
+
+            ChartArea ca = chart1.ChartAreas[0];
+
+            // Just putting Min and Max in order in case user added incorrectly
+            ca.AxisX.ScaleView.Zoom(Math.Min(min_val, max_val), Math.Max(min_val, max_val));
+            GC.Collect();
+        }
+
+        private void chart1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ChartArea ca = chart1.ChartAreas[0];
+
+            lbl_min_value.Text = ca.AxisX.Minimum.ToString();
+            lbl_max_value.Text = ca.AxisX.Maximum.ToString();
+
+            // lbl_min_value.Text = e.X.ToString();
+        }
+    }
+}
